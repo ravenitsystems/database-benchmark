e2f2a6739d8ab6530e25ee2ea4dbabe01a83a757
Nicholas Ashley <nicholas.a@freetimers.com>
2026-03-10 12:36:44 +0000

    Laravel install with datasets in resources

---
+++ app/Console/Commands/InsertComicsNumericId.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class InsertComicsNumericId extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:insert-comics-numeric-id';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        //
+    }
+}
